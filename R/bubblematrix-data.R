57a524f4d1750bb5472755acb1c2203660f2158e
Yujinhan _98 <49636371+hyj12345@users.noreply.github.com>
2021-10-29 10:23:12 +0800

    Add files via upload

--- R/bubblematrix-data.R
+++ R/bubblematrix-data.R
@@ -1,10 +1,13 @@
+
 #' @title An example data for bubble_matrix()
 #'
 #' @description These data were generated from the Manson_VITAL_protocol_NEJM_2019
 #' study.
+#'  @name example_data
 #' @docType data
-#' @name example_data
-#' @export
+#' @rdname example_data
+#'
+#' 
 #' 
 example_data<-data.frame(malca=c(0.06367852,0.06363917,0.06312858),
                          colca=c(0.004332353,0.004332353,0.004332353),
