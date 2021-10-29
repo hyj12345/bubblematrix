c6f6492554fac0b8e64f7593a406b0231be749cb
yujin <hanyujinruc@163.com>
2021-10-28 21:32:02 -0400

    pkg bubblematrix

---
+++ R/bubblematrix-data.R
@@ -0,0 +1,17 @@
+#' @title An example data for bubble_matrix()
+#'
+#' @description These data were generated from the Manson_VITAL_protocol_NEJM_2019
+#' study.
+#' @docType data
+#' @name example_data
+#' 
+NULL
+example_data<-data.frame(malca=c(0.06367852,0.06363917,0.06312858),
+                         colca=c(0.004332353,0.004332353,0.004332353),
+                         brca=c(0.01742586,0.01989593,0.01831502),
+                         prca=c(0.03420144,0.03420144,0.03420144),
+                         cancer=c(0.06367852,0.06363917,0.06312858))
+
+
+
+
